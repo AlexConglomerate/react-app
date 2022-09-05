d7bf89a0a83fd689e6c520c6e72d9ed4138444ec
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-09-05 21:37:40 +0500

    9. Рендер списка из массива | done ✅

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0)
+    const [tags, setTags] = useState(['tag1', 'tag2', 'tag3',])
     const formatCount = () => {
         return count === 0 ? "empty" : count
     }
@@ -13,17 +14,25 @@ const Counter = () => {
     }
 
     const handleIncrement = () => {
-        setCount((prevState)=>prevState + 1)
+        setCount((prevState) => prevState + 1)
     }
 
-        const handleDecrement = () => {
-        setCount((prevState)=>prevState - 1)
+    const handleDecrement = () => {
+        setCount((prevState) => prevState - 1)
     }
 
+    const handleChangeTag = () => {
+        setTags(['tag4', 'tag5'])
+    }
 
 
     return (
         <>
+            <ul>{
+                tags.map(tag => {
+                    return <li key={tag} className="btn btn-primary btn-sm m-2" onClick={handleChangeTag}>{tag}</li>
+                })
+            }</ul>
             <span className={getBadgeClasses()}>{formatCount()}</span>
             <button className='btn bg-primary btn-sm m-2' onClick={handleIncrement}>+</button>
             <button className='btn bg-primary btn-sm m-2' onClick={handleDecrement}>-</button>
