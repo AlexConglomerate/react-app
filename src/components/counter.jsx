34c484554c29b75e1902d7653c4b538e12f02b13
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-09-06 02:08:32 +0500

    11. Условный рендеринг | done ✅

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -26,15 +26,27 @@ const Counter = () => {
         setTags(prevState => prevState.filter(tag => tag != id))
     }
 
+    const renderTags = () => {
+        // return <h1>Hello</h1>
+        return (tags.length !== 0 &&
+            tags.map((tag) => (
+                <li key={tag}
+                    className="btn btn-primary btn-sm m-2"
+                    onClick={() => handleChangeTag(tag)}
+                >
+                    {tag}
+                </li>
+            ))
+        );
+    }
+
+    if (tags.length !== 0) {
+        return <ul>{renderTags()}</ul>
+    }
 
     return (
         <>
-            <ul>{
-                tags.map(tag => {
-                    return <li key={tag} className="btn btn-primary btn-sm m-2"
-                               onClick={() => handleChangeTag(tag)}>{tag}</li>
-                })
-            }</ul>
+            <ul>{renderTags()}</ul>
             <span className={getBadgeClasses()}>{formatCount()}</span>
             <button className='btn bg-primary btn-sm m-2' onClick={handleIncrement}>+</button>
             <button className='btn bg-primary btn-sm m-2' onClick={handleDecrement}>-</button>
