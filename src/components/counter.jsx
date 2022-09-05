ba6b58538bab75f5f8c8ef46b3d876cb070fd1c3
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-09-06 01:42:49 +0500

    10. Передача аргументов событий | done ✅

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -21,8 +21,9 @@ const Counter = () => {
         setCount((prevState) => prevState - 1)
     }
 
-    const handleChangeTag = () => {
-        setTags(['tag4', 'tag10'])
+    const handleChangeTag = (id) => {
+        // удаляем элемент со страницы
+        setTags(prevState => prevState.filter(tag => tag != id))
     }
 
 
@@ -30,7 +31,8 @@ const Counter = () => {
         <>
             <ul>{
                 tags.map(tag => {
-                    return <li key={tag} className="btn btn-primary btn-sm m-2" onClick={handleChangeTag}>{tag}</li>
+                    return <li key={tag} className="btn btn-primary btn-sm m-2"
+                               onClick={() => handleChangeTag(tag)}>{tag}</li>
                 })
             }</ul>
             <span className={getBadgeClasses()}>{formatCount()}</span>
