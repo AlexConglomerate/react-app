f9cbbb8b8e5adf808fe1f3960e354c5fae9b414f
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-09-17 06:01:20 +0500

    2. Рефакторинг кода

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0)
-    const [tags, setTags] = useState(['tag1', 'tag2', 'tag3',])
+
     const formatCount = () => {
         return count === 0 ? "empty" : count
     }
@@ -21,28 +21,6 @@ const Counter = () => {
         setCount((prevState) => prevState - 1)
     }
 
-    const handleChangeTag = (id) => {
-        // удаляем элемент со страницы
-        setTags(prevState => prevState.filter(tag => tag != id))
-    }
-
-    const renderTags = () => {
-        // return <h1>Hello</h1>
-        return (tags.length !== 0 &&
-            tags.map((tag) => (
-                <li key={tag}
-                    className="btn btn-primary btn-sm m-2"
-                    onClick={() => handleChangeTag(tag)}
-                >
-                    {tag}
-                </li>
-            ))
-        );
-    }
-
-    if (tags.length !== 0) {
-        return <ul>{renderTags()}</ul>
-    }
 
     return (
         <>
