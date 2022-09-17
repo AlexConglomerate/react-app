c1663988e0e45d44a206da61e9dc204f308a0a55
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-09-17 12:17:58 +0500

    3. Композиция компонентов | Задание #1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 
 const Counter = (props) => {
 
-    // const [value, setValue] = useState(props.value)
     const {value} = props
     const formatValue = () => {
         return value === 0 ? "empty" : value
@@ -14,27 +13,13 @@ const Counter = (props) => {
         return classes
     }
 
-    const handleIncrement = () => {
-        console.log(`handleIncrement`)
-        // props.value = props.value + 1  // не будет работать, т.к. props.value доступна только для чтения
-        // Мы может изменять только useState, а не пропсы, которые задаём снаружи
-        // setValue((prevState) => prevState + 1)
-    }
-
-    const handleDecrement = () => {
-        console.log(`handleDecrement`)
-        // setValue((prevState) => prevState - 1)
-    }
-
 
     return (
         <div>
             <span>{props.name}</span>
             <span className={getBadgeClasses()}>{formatValue()}</span>
-            <button className='btn bg-primary btn-sm m-2' onClick={handleIncrement}>+</button>
-            <button className='btn bg-primary btn-sm m-2' onClick={handleDecrement}>-</button>
-            {/*Мы не можем из дочернего компонента изменять родительский
-            Поэтому мы передаём в дочерний компонент метод, из родительского компонента*/}
+            <button className='btn bg-primary btn-sm m-2' onClick={() => props.onIncrement(props.id)}>+</button>
+            <button className='btn bg-primary btn-sm m-2' onClick={() => props.onDecrement(props.id)}>-</button>
             <button
                 className="btn btn-danger btn-sm m-2"
                 onClick={() => props.onDelete(props.id)}
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Counter from "./counter";
 
+
 const CountersList = () => {
     const initialState = [
         {id: 0, value: 0, name: "iPhone"},
@@ -17,21 +18,20 @@ const CountersList = () => {
         setCounters(newCounters)
     }
 
-    const handleReset = () => {
-        // тут мы меняем просто инициализацию, но не переписываем новый компонент
-        setCounters(initialState)
+    const changeValue = (id, number) => {
+        const updateArr = counters.map(i => {
+            if (i.id === id) {
+                i.value += number
+            }
+            return i
+        })
+        setCounters(updateArr)
     }
 
-    const handleUpdate = () => {
-        const updateState = [
-            {id: 0, value: 1, name: "iPhone"},
-            {id: 1, value: 4, name: "Apple"},
-            {id: 2, value: 44, name: "Tennis"},
-            {id: 3, value: 666, name: "WebStorm"},
-            {id: 4, value: 100, name: "Uzbekistan"}
-        ]
-        setCounters(updateState)
-    }
+    const onIncrement = (id) => changeValue(id, +1)
+    const onDecrement = (id) => changeValue(id, -1)
+    const handleReset = () => setCounters(initialState)
+
 
     return <>
         {counters.map(count => (
@@ -39,6 +39,8 @@ const CountersList = () => {
                 key={count.id}
                 {...count}
                 onDelete={handleDelete}
+                onIncrement={onIncrement}
+                onDecrement={onDecrement}
             />
 
         ))}
@@ -46,10 +48,6 @@ const CountersList = () => {
             className="btn btn-danger btn-sm m-2"
             onClick={() => handleReset()}
         >Reset</button>}
-        {<button
-            className="btn btn-danger btn-sm m-2"
-            onClick={() => handleUpdate()}
-        >Update</button>}
     </>
 }
 
